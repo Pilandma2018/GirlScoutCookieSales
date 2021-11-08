17257d7a91e165088f932b7ab310fbbb2c261935
Pilandma2018 <93951814+Pilandma2018@users.noreply.github.com>
2021-11-08 15:00:34 -0800

    Add files via upload

---
+++ GirlScout/src/main/java/LYT/GirlScout/CookieSales.java
@@ -0,0 +1,27 @@
+package LYT.GirlScout;
+import java.util.*;
+public class CookieSales {
+	public static void main( String[] args ) {
+		
+	int i;
+	Scanner sc = new Scanner(System.in);
+	System.out.print("Enter the number of girl scout members: ");
+	i=sc.nextInt();
+	int[] array = new int[i];
+	System.out.println("Enter the amount of boxes sold: ");
+	for(int j=0; j<i; j++)
+		{
+		array[j] = sc.nextInt();
+		}
+	
+        }
+/*	System.out.println("Array elements are: ");
+	
+	for (int j=0; j<i; j++)
+	{
+		System.out.println(array[j]);
+			}
+			*/
+
+}
+
