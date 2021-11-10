985535ab6c8a1f585529bea6e819871bd1aa29ea
Pilandma2018 <93951814+Pilandma2018@users.noreply.github.com>
2021-11-09 17:15:43 -0800

    Add files via upload
    runs and performs properly

--- GirlScout/src/main/java/LYT/GirlScout/CookieSales.java
+++ GirlScout/src/main/java/LYT/GirlScout/CookieSales.java
@@ -3,25 +3,36 @@ import java.util.*;
 public class CookieSales {
 	public static void main( String[] args ) {
 		
-	int i;
-	Scanner sc = new Scanner(System.in);
-	System.out.print("Enter the number of girl scout members: ");
-	i=sc.nextInt();
-	int[] array = new int[i];
-	System.out.println("Enter the amount of boxes sold: ");
-	for(int j=0; j<i; j++)
-		{
-		array[j] = sc.nextInt();
-		}
+		int i;
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the number of girl scout members: ");
+		i=sc.nextInt();
+		int[] boxArray = new int[i];
+		int[] categoryArray = new int[5];
+		System.out.println("Enter the amount of boxes sold: ");
+		for(int j=0; j<i; j++)
+			{
+			boxArray[j] = sc.nextInt();
+	if((boxArray[j]>=0) && (boxArray[j]<=10)) {
+		categoryArray[0]++;
+		System.out.println("Boxes sold between 0 and 10: " + categoryArray[0]);
+	}else if(boxArray[j] <=20) {
+		categoryArray[1]++;
+		System.out.println("Boxes sold between 11 and 20: " + categoryArray[1]);
+	}else if(boxArray[j] <=30) {
+		categoryArray[2]++;
+		System.out.println("Boxes sold between 21 and 30: " + categoryArray[2]);
+	}else if(boxArray[j] <= 40) {
+		categoryArray[3]++;
+		System.out.println("Boxes sold between 31 and 40: " + categoryArray[3]);
+	}else if (boxArray[j] >=41) {
+		categoryArray[4]++;
+		System.out.println("Boxes sold over 40: " + categoryArray[4]);
+	}
 	
-        }
-/*	System.out.println("Array elements are: ");
-	
-	for (int j=0; j<i; j++)
-	{
-		System.out.println(array[j]);
-			}
-			*/
+			
 
+}
+	}
 }
 
